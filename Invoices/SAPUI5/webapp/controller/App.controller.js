276c9e3986287f56e56378f770fe4b822cc9745c
dagoneth <dsgcba@hotmail.com>
2021-07-11 23:37:41 +0000

    commit1

--- Invoices/SAPUI5/webapp/controller/App.controller.js
+++ Invoices/SAPUI5/webapp/controller/App.controller.js
@@ -1,27 +1,15 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
 ],
     /**
      * 
      * @param {typeof sap.ui.core.mvc.Controller} Controller 
-     * @param {typeof sap.ui.core.mvc.MessageToast} MessageToast 
-     * @param {typeof sap.ui.model.resource.ResourceModel} ResourceModel 
      */
-    function (Controller, MessageToast, Models, ResourceModel) {
+    function (Controller, MessageToast) {
         "use strict";
         return Controller.extend("logali_group.SAPUI5.controller.App", {
             onInit: function () {
 
             },
-
-            onShowHello: function () {
-                //read text from i18n model
-                var oBundle = this.getView().getModel("i18n").getResourceBundle();
-                //read propert from data model
-                var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-                var sMsg = oBundle.getText("helloMsg",[sRecipient]);
-                MessageToast.show(sMsg);
-            }
         });
     });
